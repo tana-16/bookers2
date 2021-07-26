010e15041929dd5b9b40c347c1de4d6ca1cdbe52
tana-16 <tanahiro1697@gmail.com>
2021-07-26 09:49:58 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  
-  devise_for :user
+
+  devise_for :users
   # get 'homes/about' => 'homes#show'
   # get 'users/show'
 
   # resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
   resources :users, only: [:index, :show, :edit, :update]
-  
+
 
   resources :books
   # post 'books' => 'books#create'
